d7696de7cfdaf368bfbbb86553dd39e752c6fd3d
muidstim <tim.gir@mahidol.edu>
2020-03-10 00:36:12 +0700

    Update Pentagon.java

--- Pentagon.java
+++ Pentagon.java
@@ -7,6 +7,11 @@ public class Pentagon{
     return 0.5 * 5 * side * apothem;
   }
   
+  public static double volumeOfPentagonPrism(double area, double height){
+    return area * height;
+  }
+  
+
   public static void main(String[] args){
     System.out.println("Enter the side length and the apothem: ");
 
@@ -14,6 +19,13 @@ public class Pentagon{
     
     double area = areaOfPentagon(s.nextDouble(), s.nextDouble());
     
-    System.out.format("The area of the regular pentagon is %f cm^2", area);
+    System.out.format("The area of the regular pentagon is %f cm^2\n", area);
+
+    System.out.println("Enter the height of the pentagon prism: ");
+
+    double volume = volumeOfPentagonPrism(area, s.nextDouble());
+
+    System.out.format("The volume of the regular pentagon prism is %f cm^3\n", volume);
+
   }
 }
